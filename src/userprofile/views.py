c612f015348d2832abd0d6fe5b0de88d754266a1
Simeon Kostadinov <simeon.kostadinoff@gmail.com>
2016-07-16 13:55:32 +0100

    Fixed nabber showing for profile.html

--- src/userprofile/views.py
+++ src/userprofile/views.py
@@ -2,24 +2,20 @@ from django.shortcuts import render, render_to_response, redirect
 from django.shortcuts import HttpResponseRedirect, Http404, HttpResponse
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
-from django.core.context_processors import csrf
 
 # Create your views here.
 from userprofile.forms import UserForm
 from userprofile.forms import UserProfileForm
 
+@login_required
 def userprofile(request):
-    print('In userprofile view')
     if request.method == 'POST':
-        print('when the request is POST')
         user_form = UserForm(request.POST, instance=request.user)
         user_profile = UserProfileForm(request.POST, request.FILES, instance=request.user.profile)
         if user_form.is_valid() and user_profile.is_valid():
             user_form.save()
             user_profile.save()
-            print('Hello from valid!')
     else:
-        print('NOT POST request')
         user_form = UserForm(instance=request.user,
             initial={
                 'first_name': request.user.first_name,
@@ -30,4 +26,6 @@ def userprofile(request):
         profile = user.profile
         user_profile = UserProfileForm(instance=profile)
 
-    return render_to_response('userprofile/profile.html', {'user_form': user_form, 'user_profile': user_profile}, context_instance=RequestContext(request))
+    role_path = ''.join([request.session.get('role'), '/base.html'])
+
+    return render_to_response('userprofile/profile.html', {'user_form': user_form, 'user_profile': user_profile, 'user_role_path':role_path}, context_instance=RequestContext(request))
