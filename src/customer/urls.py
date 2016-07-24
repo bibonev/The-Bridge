373ebf331ecfd89b332ae36ed482fad948f4ca91
Simeon Kostadinov <simeon.kostadinoff@gmail.com>
2016-07-24 18:03:53 +0100

    Updated css files + added add friend button

--- src/customer/urls.py
+++ src/customer/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
-    url(r'organisations/(?P<pk>\d+)/$', views.organisation_details, name='organisation_details')
+    url(r'organisations/(?P<pk>\d+)/$', views.organisation_details, name='organisation_details'),
+    url(r'organisations/sent/$', views.friendship_add_organisation, name='organisation_request'),
 ]
--- src/customer/views.py
+++ src/customer/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from developer.models import Organisation
+from friendship.exceptions import AlreadyExistsError
+from partnership.models import Friend, FriendshipRequest
 from developer import models
-# Create your views here.
+
+get_friendship_context_object_name = lambda: getattr(settings, 'FRIENDSHIP_CONTEXT_OBJECT_NAME', 'user')
+get_friendship_context_object_list_name = lambda: getattr(settings, 'FRIENDSHIP_CONTEXT_OBJECT_LIST_NAME', 'users')
 
 @login_required
 def home_page(request):
@@ -17,5 +24,21 @@ def organisation_details(request, pk):
     organisation = get_object_or_404(models.Organisation, pk=pk)
 
     return render(request, 'customer/organisation_details.html', {'org':organisation})
+
+@login_required
+def friendship_add_organisation(request):
+    print('Friend added')
+    if request.method == 'POST':
+        to_user = Organisation.objects.get(pk=request.POST.get('hidden_org_id'))
+        from_user = request.user
+        try:
+            Friend.objects.add_friend(from_user, to_user)
+            print('Been here, done that')
+        except AlreadyExistsError as e:
+            print('Exception when adding a friend ' + e)
+        else:
+            return HttpResponse('You added a friend')
+
+    #return render(request, 'customer/organisation_details.html')
 
 
