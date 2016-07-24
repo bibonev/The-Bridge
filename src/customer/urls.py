e2f04cd3d2bb0ec4f84262b38f79191749027ffc
Boyan Bonev <boyan.bonev@hotmail.com>
2016-07-24 22:47:09 +0100

    Finished with search organisations.

--- src/customer/urls.py
+++ src/customer/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'organisations/(?P<pk>\d+)/$', views.organisation_details, name='organisation_details'),
-    url(r'organisations/search/(?P<title>[\w\-]+)/$', views.organisation_details_search_by_title, name='organisation_details_search_by_title'),
 ]
--- src/customer/views.py
+++ src/customer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.contrib.auth.decorators import login_required
 from developer import models
 # Create your views here.
@@ -9,18 +9,16 @@ def home_page(request):
     request.session['role'] = 'customer'
     request.session.modified = True
     organisations = models.Organisation.objects.all()
+    query = request.GET.get('searchTerm')
+
+    if query:
+        organisations = organisations.filter(title__icontains=query)
 
     return render(request, 'customer/home.html', {'organisations':organisations})
 
 @login_required
 def organisation_details(request, pk):
     organisation = get_object_or_404(models.Organisation, pk=pk)
-
-    return render(request, 'customer/organisation_details.html', {'org':organisation})
-
-@login_required
-def organisation_details_search_by_title(request, title):
-    organisation = get_object_or_404(models.Organisation, title=title)
 
     return render(request, 'customer/organisation_details.html', {'org':organisation})
 
