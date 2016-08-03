8b9a4c06d2ac9e6d932661c800fc8251d523063d
Simeon Kostadinov <simeon.kostadinoff@gmail.com>
2016-08-03 12:51:03 +0100

    Fixed issues with nabber and slider images.

--- src/app/static/js/navbar-scroll.js
+++ src/app/static/js/navbar-scroll.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > startchange) {
-          $('header.navbar-default').css({'background-color': '#029acf', 'border-bottom':'1px solid #c4c4c4 !important'});
+          $('header').css({'background-color': '#029acf', 'border-bottom':'1px solid #c4c4c4'});
        } else {
-          $('header.navbar-default').css({'background-color': 'transparent', 'border-bottom':'0 !important'});
+          $('header').css({'background-color': 'transparent', 'border':'0'});
        }
    });
 });
