7a634746197f21463f6850d80b9160a25df47ec6
Simeon Kostadinov <simeon.kostadinoff@gmail.com>
2016-10-20 17:45:42 +0100

    Removed not working search field.

--- src/app/static/js/extraJSFuncs.js
+++ src/app/static/js/extraJSFuncs.js
@@ -4,20 +4,4 @@ $(document).ready(function(){
     } else {
         $(".secondary-navigation a[name='dashboard']").addClass("active")
     }
-
-    $('#search_field').keydown(function(e){
-        if (e.keyCode === 13) {
-            e.preventDefault();
-            searchTerm = $(this).val()
-            if(searchTerm !=''){
-                $(this).val('');
-                window.location.replace("http://localhost:8000/organisations/");
-                localStorage.setItem("searchTerm",searchTerm);
-            }
-        }
-    })
-    if($("input[name='searchTerm']").length > 0){
-        var searchTerm = localStorage.getItem("searchTerm")
-        $("input[name='searchTerm']").val(searchTerm)
-    }
 });
