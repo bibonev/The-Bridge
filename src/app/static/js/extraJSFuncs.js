9134cd5b76600416be11ac2c486585209e3c3067
Simeon Kostadinov <simeon.kostadinoff@gmail.com>
2016-11-18 23:39:12 +0000

    Style send request form.

--- src/app/static/js/extraJSFuncs.js
+++ src/app/static/js/extraJSFuncs.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){  
+    // make the current anchor tag showing active
     if (location.pathname !== '/') {
         $(".secondary-navigation a[href*='" + location.pathname.split("/")[1] + "']").addClass("active");
     } else {
         $(".secondary-navigation a[name='dashboard']").addClass("active");
     }
 });
+
+// auto resize each textarea with class 'autoExpand'
+$(document)
+    .one('focus.autoExpand', 'textarea.autoExpand', function(){
+        var savedValue = this.value;
+        this.value = '';
+        this.baseScrollHeight = this.scrollHeight;
+        this.value = savedValue;
+    })
+    .on('input.autoExpand', 'textarea.autoExpand', function(){
+        var minRows = this.getAttribute('data-min-rows')|0, rows;
+        this.rows = minRows;
+        rows = Math.ceil((this.scrollHeight - this.baseScrollHeight) / 17);
+        this.rows = minRows + rows;
+    });
