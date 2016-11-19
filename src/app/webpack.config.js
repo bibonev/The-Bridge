f73fec24e246b3652a1a622d2f3f66f4304daac4
Boyan Bonev <boyan.i.bonev@gmail.com>
2016-11-19 19:48:42 +0000

    Bit closer.

--- src/app/webpack.config.js
+++ src/app/webpack.config.js
@@ -7,40 +7,32 @@ module.exports = {
   entry: {
     // Add as many entry points as you have container-react-components here
     organisation_search: './reactjs/modules/OrganisationSearch/index',
-    posts_dashboard: './reactjs/modules/PostsDashboard/index',
-    posts_organisation: './reactjs/modules/PostsOrganisation/index',
-    review_organisation: './reactjs/modules/ReviewOrganisation/index',
-    navbar: './reactjs/modules/Navbar/index',
+    // posts_dashboard: './reactjs/modules/PostsDashboard/index',
+    // posts_organisation: './reactjs/modules/PostsOrganisation/index',
+    // review_organisation: './reactjs/modules/ReviewOrganisation/index',
+    // navbar: './reactjs/modules/Navbar/index',
     vendors: ['react'],
   },
 
   output: {
       path: path.resolve('./static/bundles/local/'),
-      filename: "[name].js"
-  },
-  
-  node: {
-    fs: "empty"
+      filename: "[name]-[hash].js"
   },
 
-  externals: [
-  ], // add all vendor libs
+  externals: [], // add all vendor libs
 
   plugins: [
     new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
   ], // add all common plugins here
 
   module: {
-    loaders: [ 
-      { test: /\.json$/, loader: "json-loader"},
-      { test: /\.js$/, loader: 'babel-loader'}
+    loaders: [
+      //  { test: /\.json$/, loader: "json-loader"},
+      //  { test: /\.js$/, loader: 'babel-loader'}
     ] // add all common loaders here
   },
 
   resolve: {
-    alias: {
-      'react': path.join(__dirname, 'node_modules', 'react')
-    },
     modulesDirectories: ['node_modules', 'bower_components'],
     extensions: ['', '.js', '.jsx', '.json']
   },
